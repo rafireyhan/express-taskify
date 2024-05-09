7edbce709cd061e11df3dd1d358a22442febccbd
rafireyhan <redan215@gmail.com>
2024-05-09 11:59:16 +0700

    feat(main): initialize project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+ 
 require("dotenv").config({path: `.env`});
 
 //Middleware
